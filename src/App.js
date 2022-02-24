f89eb78baceaea4d7cfbb408e49c197e8717a581
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-24 16:07:13 -0500

    Se añadio la funcion de que recorra la tabla ingresada en el body de la tabla y adicional la pinte dinamicamente segun la informacion que ella contenga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import UserTable from './components/UserTable';
+//libreria para manejar Id de una forma mas simple
+import {v4 as uuidv4 } from 'uuid';
+
 function App() {
+
+  //constante lista que tiene informacion de los usuarios array de objetos, el uuidv4() es una funcion que nos genera los id
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
+
+  //Estado de la tabla userData, users es el nombre del estado, setUsers es el nombre con el que podemos llamar al estado de la tabla para modificarla y dentro del useState ingresamos el valor para inicializarlo en este caso vamos a inicializar  la tabla de objetos userData 
+  const [users, setUsers] = useState(usersData)
+
   return (
 
     //Maquetacion inicial
@@ -13,8 +27,9 @@ function App() {
       <div className="flex-large">
         <h2>View users</h2>
       
-        {/* Añadimos la tabla componente en la columna View users*/}
-        <UserTable/> 
+        {/* Añadimos la tabla componente en la columna View users
+        Adicional con users le pasamos la informacion de la userData como un props al componente UserTable*/}
+        <UserTable users={users}/> 
       </div>
     </div>
   </div>
--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-function UserTable() {
+//props es el valor pasado por parametro en donde se esta implementando el componente en este caso App.js
+
+function UserTable(props) {
   return (
 
     //Tabla en HTML
@@ -13,14 +15,28 @@ function UserTable() {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Name data</td>
-        <td>Username data</td>
-        <td>
-          <button className="button muted-button">Edit</button>{" "}
-          <button className="button muted-button">Delete</button>
-        </td>
-      </tr>
+        {
+            //condicional para preguntar si la lista viene llena o vacia su estructura es la siguiente props.user.length > 0 ? verdadero : (negativa). se llama if de linea
+            props.users.length > 0 ?
+        //funcion para recorrer y pintar en el tbody iterando las veces en las que esta inicializada la lista. Luego pasamos la informacion a dinamica se va a ir cambiando con el nombre del props seguido de su propiedad y la actualiza en la tabla
+            props.users.map(user=>(
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>
+                    <button className="button muted-button">Edit</button>{" "}
+                    <button className="button muted-button">Delete</button>
+                    </td>
+                </tr>
+            
+            )) : ( //Despues de los dos puntos viene la negativa del if de linea
+                <tr>
+                    <td colSpan={3}>No Users</td>
+                </tr>
+
+            )
+        }
+      
     </tbody>
   </table>
 
