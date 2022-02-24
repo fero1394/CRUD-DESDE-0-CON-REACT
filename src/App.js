89f49d3488f862959c0b71d4631034a19bd613d8
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-24 18:40:49 -0500

    En este punto tenemos funcional el boton editar usuario el boton delete el boton añadir nuevo usuario, la tabla responde a las acciones

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import UserTable from './components/UserTable';
 //libreria para manejar Id de una forma mas simple
 import {v4 as uuidv4 } from 'uuid';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 
 function App() {
 
@@ -15,6 +17,42 @@ function App() {
   //Estado de la tabla userData, users es el nombre del estado, setUsers es el nombre con el que podemos llamar al estado de la tabla para modificarla y dentro del useState ingresamos el valor para inicializarlo en este caso vamos a inicializar  la tabla de objetos userData 
   const [users, setUsers] = useState(usersData)
 
+
+
+
+
+
+  //Se crea funcion AGREGAR NUEVOS USUARIOS recibe como parametro un usuario(objetos con sus propiedades id,name,username), luego generamos un id para este usuario nuevo y modificamos el array para añadirle el nuevo usuario,los tres puntos que van antes del nombre de la lista significa que se va a concatenar a el array el user
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+  //el usuario que recibe la funcion addUser (user) lo capturamos en el formulario que esta en el componente AddUserForm
+
+
+
+//Funcion para eliminar usuarios
+const deleteUser = id => {
+
+  //se genera un nuevo array excluyendo el valor identificado con el id que pasamos por parametro
+  const arrayFiltrado = users.filter(user => user.id !== id)
+  
+  setUsers(arrayFiltrado)
+}
+
+
+//Funcion para editar Usuarios
+const [editing, setEditing] = useState(false);
+
+
+
+
+
+
+
   return (
 
     //Maquetacion inicial
@@ -22,14 +60,38 @@ function App() {
     <h1>CRUD App with Hooks</h1>
     <div className="flex-row">
       <div className="flex-large">
-        <h2>Add user</h2>
+
+      {/**Esto es un condicional que pregunta si el botton editar fue presionado si es asi ejecuta el primer bloque de codigo para editar usuario y muestra todo el bloque en pantalla por eso es importante que este dentro de un contenedor div ya que se va a ejecutar todo, si no es asi ejecuta el 'else' segundo bloque de codigo */}
+      {
+        editing ? (
+          <div>
+
+            <h2>Edit User</h2>
+            <EditUserForm/>
+
+          </div>
+        ) : (
+          <div>
+          <h2>Add user</h2>
+        <AddUserForm addUser={addUser}/>
+        </div>
+        )
+
+      }
+        
+
+
+        
       </div>
       <div className="flex-large">
         <h2>View users</h2>
       
         {/* Añadimos la tabla componente en la columna View users
         Adicional con users le pasamos la informacion de la userData como un props al componente UserTable*/}
-        <UserTable users={users}/> 
+        <UserTable 
+        users={users} 
+        deleteUser={deleteUser} 
+        setEditing={setEditing}/> 
       </div>
     </div>
   </div>
---
+++ src/components/EditUserForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useForm} from 'react-hook-form'
+
+function EditUserForm(props) {
+
+    //react-hook-form es una 'libreria' para manejar fomularios y su ventaja es poder validar formularios de una forma muy sencilla la funcion handleSubmit es la encargada de hacer las validaciones en cada input, con el register le vamos a decir que queremos validar de el input ya sea minimo de palabras o maximo e.t.c
+    //const {register, errors, handleSubmit} = useForm();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    //funcion para capturar la informacion del formulario y convertirla en objeto para asi enviarla por addUser para que la añada al array
+    const onSubmit = (data, e) => {
+        //console.log(data)
+        props.addUser(data)
+
+        //limpia los campos despues hacer onSubmit
+        e.target.reset();
+    }
+
+  return (
+      //el onSubmit llama a la funcion para pasarle por parametro el handleSubmit
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <input type="text" name="name" {...register("name", {
+             required: true})}/>
+
+        {/*manejo de errores pregunta si hay errores si es asi pregunta si es con name si es asi despliega el mensaje configurado */}
+
+        <div>
+            {errors?.name && <span>Campo Obligatorio</span>}
+        </div>
+
+
+        <label>Username</label>
+        <input type="text" name="username" {...register("username", {
+             required: true})} />
+        <div>
+            {errors?.username && <span>Campo Obligatorio</span>}
+        </div>
+        
+        <button>Edit User</button>
+  </form>
+  )
+}
+
+export default EditUserForm
